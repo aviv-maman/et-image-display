a284a8ac16ad315bd28c55dc15bf4b941e5d14c5
Aviv Maman <awiwmaman@outlook.com>
2024-09-02 01:03:07 +0300

    Update search.ts

--- src/routes/search.ts
+++ src/routes/search.ts
@@ -2,29 +2,6 @@ import express from 'express';
 
 const router = express.Router();
 const API_KEY = process.env.PIXABAY_API_KEY;
-const categories = [
-  'all',
-  'backgrounds',
-  'fashion',
-  'nature',
-  'science',
-  'education',
-  'feelings',
-  'health',
-  'people',
-  'religion',
-  'places',
-  'animals',
-  'industry',
-  'computer',
-  'food',
-  'sports',
-  'transportation',
-  'travel',
-  'buildings',
-  'business',
-  'music',
-];
 
 router.get('/', async (req, res) => {
   const { query, image_type, category, page, pageSize } = req.query;
